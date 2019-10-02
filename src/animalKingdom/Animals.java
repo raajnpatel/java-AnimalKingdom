b4b2b493ab299589435b2576af27d7123d9b2c38
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-02 11:52:20 -0700

    abstract class for animals complete

--- src/animalKingdom/Animals.java
+++ src/animalKingdom/Animals.java
@@ -2,5 +2,42 @@ package  animalKingdom;
 
 public abstract class Animals
 {
+    private static int animalID = 0;
+    int number;
+    String name;
+    int yearDiscovered;
+    int fuel;
 
+    public Animal(int number, String name, int yearDiscovered, int fuel)
+    {
+        animalID++;
+        number = animalID;
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
+        this.fuel = fuel;
+    }
+
+    public abstract String move();
+    public abstract String breathe();
+    public abstract String reproduce();
+
+    public int getFuel()
+    {
+        return fuel;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getYearDiscovered()
+    {
+        return yearDiscovered;
+    }
 }
