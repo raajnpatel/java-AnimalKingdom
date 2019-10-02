3dc01893f64ac2fa7ef05a54e5910655ccde3db2
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-02 12:25:26 -0700

    checker created for future use

---
+++ src/animalKingdom/Checker.java
@@ -0,0 +1,6 @@
+package animalKingdom;
+
+//checks if passed in value matches an Animal. returns true if it does.
+public interface Checker {
+    boolean test(Animals a);
+}
--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -6,6 +6,14 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Mammals
+
+
+        // Birds
+
+
+        // Fish
+
 
     }
 
