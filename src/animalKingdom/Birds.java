e3f1b7be20fc3e0f7653532fb30b58b0ffb9da18
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-02 12:01:11 -0700

    Bird class complete

--- src/animalKingdom/Birds.java
+++ src/animalKingdom/Birds.java
@@ -1,1 +1,32 @@
 package  animalKingdom;
+
+public class Bird extends Animals
+{
+
+    public Bird(String name, int fuel, int yearDiscovered)
+    {
+        super(name, fuel, yearDiscovered);
+    }
+
+    @Override
+    public String move() {
+        return "fly";
+    }
+
+    @Override
+    public String breathe() {
+        return "lungs";
+    }
+
+    @Override
+    public String reproduce() {
+        return "eggs";
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "Bird{" + "name=" + name + ", fuel=" + fuel + ", year discovered=" + yearDiscovered + '}';
+    }
+}
+
+}
